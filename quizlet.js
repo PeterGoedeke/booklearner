d7f3adba1ccb5fd9bee472c3dcfb9f101bc7b0a4
Peter Goedeke <peterbgoedeke@gmail.com>
2021-01-11 23:19:10 +1300

    feat: requests to webit

---
+++ quizlet.js
@@ -0,0 +1,23 @@
+require('dotenv').config()
+const axios = require('axios')
+
+const woerter = new Set(require('./routes/woerter.json'))
+const words = require('german-words')
+const list = require('german-words-dict')
+
+async function makeRequest(q) {
+    console.log('yeet');
+    const response = await axios.get(process.env.WEBIT_URL, {
+        params: {
+            q,
+            from: 'de',
+            to: 'en'
+        },
+        // required headers for rapidapi
+        headers: {
+            'x-rapidapi-host': 'webit-language.p.rapidapi.com',
+            'x-rapidapi-key': process.env.WEBIT_API_KEY
+        }
+    })
+    return response.data
+}
