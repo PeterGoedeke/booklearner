a670158acd90858cb658f4809c3d47e3ffc66778
PeterGoedeke <peterbgoedeke@gmail.com>
2021-01-12 16:43:53 +1300

    feat: added caching

--- quizlet.js
+++ quizlet.js
@@ -1,11 +1,38 @@
 require('dotenv').config()
 const axios = require('axios')
 
+// need to account for when the dictionary has the word but the api does not
+
+const { partition, is, splitEvery } = require('ramda')
+
 const woerter = new Set(require('./woerter.json'))
 const words = require('german-words')
 const list = require('german-words-dict')
 
+const mongoose = require('mongoose')
+const Word = mongoose.model('Word')
+const ApiResponse = mongoose.model('ApiResponse')
+
+const addArticle = w => {
+    try {
+        const gender = words.getGenderGermanWord(null, list, w)
+        if (gender == 'M') {
+            return 'der ' + w
+        }
+        else if (gender == 'F') {
+            return 'die ' + w
+        }
+        else {
+            return 'das ' + w
+        }
+    }
+    catch (e) {
+        return w
+    }
+}
+
 async function makeRequest(q) {
+    console.log('making the request!', q)
     const response = await axios.get(process.env.WEBIT_URL, {
         params: {
             q,
@@ -18,16 +45,19 @@ async function makeRequest(q) {
             'x-rapidapi-key': process.env.WEBIT_API_KEY
         }
     })
+    const apiResponse = new ApiResponse(response.data)
+    apiResponse.save().catch(console.log)
+
     return response.data
 }
 
 async function makeRequests(qs) {
     const responses = await Promise.all(qs.map(makeRequest))
-    const results = response.map(parse)
+    const results = responses.map(parser.run)
     return results.join('\n')
 }
 
-const tstrs = s => {
+const qs = (function() {
 const uniq = arr => Array.from(new Set(arr))
 
     const listwords = s =>
@@ -37,19 +67,39 @@ const uniq = arr => Array.from(new Set(arr))
         )
     .filter(w => woerter.has(w))
         
-    const split = a => {
-        const rs = []
-        let i = -1
-        for (const word of a) {
-            if (!rs[i] || rs[i].length + word.length + 1 > 10) {
-                rs[++i] = word
-                // console.log(word);
+    // const split = a => {
+    //     const rs = []
+    //     let i = -1
+    //     for (const word of a) {
+    //         if (!rs[i] || rs[i].length + word.length + 1 > 1000) {
+    //             rs[++i] = word
+    //             // console.log(word);
+    //         }
+    //         else {
+    //             rs[i] += ',' + word
+    //         }
+    //     }
+    //     return rs
+    // }
+    const split = a => splitEvery(10, a).map(qs => qs.join(','))
+
+    const thing = w => w.text + ',' + w.translation
+
+    const run = async s => {
+        const words = listwords(s)
+        const results = await Promise.all(words.map(
+            async word => {
+                const response = await Word.findOne({ source: 'de', dest: 'en', text: addArticle(word) })
+                return response == null ? word : response
             }
-            else {
-                rs[i] += ',' + word
+        ))
+        const parts = partition(is(String), results)
+        return [split(parts[0]), parts[1].map(thing).join('\n')]
             }
+    return {
+        run
         }
-        return rs
+})()
     }
     return split(listwords(s))
 }
