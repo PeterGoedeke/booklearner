54de57829ab4572e7626ca8a452e3694ffb7cbf9
PeterGoedeke <peterbgoedeke@gmail.com>
2021-01-14 16:45:20 +1300

    refactor: article adding

---
+++ controllers/addArticle.js
@@ -0,0 +1,16 @@
+const de = (function() {
+    const words = require('german-words')
+    const list = require('german-words-dict')
+
+    const genders = { M: 'der', F: 'die', N: 'das' }
+    return w => {
+        try {
+            return genders[words.getGenderGermanWord(null, list, w)] + ' ' + w
+        }
+        catch (e) { return w }
+    }
+})()
+
+module.exports = {
+    de
+}
