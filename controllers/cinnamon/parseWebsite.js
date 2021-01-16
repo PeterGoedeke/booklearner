4f30d5ad41aa8a813af58c3db036010c54c1e71c
Peter Goedeke <peterbgoedeke@gmail.com>
2021-01-17 11:57:07 +1300

    feat: added website parsing

---
+++ controllers/cinnamon/parseWebsite.js
@@ -0,0 +1,24 @@
+// library dependencies
+const axios = require('axios')
+const { JSDOM } = require('jsdom')
+
+const getTextNodes = dom => {
+    const body = dom.window.document.querySelector('body')
+    const nodeFilter = dom.window.NodeFilter.SHOW_TEXT
+    const walker = dom.window.document.createTreeWalker(body, nodeFilter, null, false)
+
+    let n, a = []
+    while (n = walk.nextNode()) {
+        a.push(n)
+    }
+    return a
+}
+
+const getWebsiteText = async url => {
+    const response = await axios.get(url)
+    const dom = new JSDOM(response.data)
+    const text = getTextNodes(dom).map(node => node.textContent).join('\n')
+    return text
+}
+
+module.exports = getWebsiteText
