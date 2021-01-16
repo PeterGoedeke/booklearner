ef93feb7ead1e43b5d150cf0305c98981f7003f2
Peter Goedeke <peterbgoedeke@gmail.com>
2021-01-17 11:57:48 +1300

    feat: input choice refactoring and url input functional

--- controllers/index.js
+++ controllers/index.js
@@ -20,60 +20,57 @@ function translatePDF(req, res) {
             })
         }
         
-        const test = parseWebsite(fields.url)
-
-
-        if (fields.text && files.file.type == 'application/pdf') {
+        const inputs = [fields.text, files.file.type == 'application/pdf', fields.url].filter(i => i)
+        if (inputs.length == 0) {
             return res.status(400).render('error', {
-                message: 'Either fill out the text field or submit a pdf',
-                error: {
-                    status: 400,
-                    stack: 'Cannot accept both text and pdf file.'
-                }
-            })
-        }
-        if (!fields.text && !files.file) {
-            return res.status(400).render('error', {
-                message: 'Either add text to be translated to the text field or submit a pdf',
+                message: 'Either submit a pdf, add text to be translated to the text field, or enter a url in the url field',
                 error: {
                     status: 400,
                     stack: 'Submit something to be translated!'
                 }
             })
         }
-
-        if (fields.text) {
-            const vocabulary = await translate(fields.text, fields.source, fields.dest)
-            return res.status(200)
-                    .attachment(`vocabulary.csv`)
-                    .send(vocabulary)
+        if (inputs.length > 1) {
+            return res.status(400).render('error', {
+                message: 'Only one submission method may be used simultaneously.',
+                error: {
+                    status: 400,
+                    stack: 'Either submit a pdf, enter text, or enter a url.'
+                }
+            })
         }
-        else {
-            const toTranslate = files.file
-            if (!toTranslate || toTranslate.type != 'application/pdf') {
-                return res.status(400).render('error', {
-                    message: 'Either no file was submitted or the format of the submitted file was not supported.',
-                    error: {
-                        status: 400,
-                        stack: 'Supported file types right now are limited to pdf files.'
-                    }
-                })
-            }
-    
+        
+        let vocabulary
+        if (files.file.type == 'application/pdf') {
             fs.readFile(toTranslate.path, async (err, data) => {
                 try {
                     const result = await pdf(data)
-                    const vocabulary = await translate(result.text, fields.source, fields.dest)
-                    return res.status(200)
-                        .attachment(`vocabulary.csv`)
-                        .send(vocabulary)
+                    vocabulary = await translate(result.text, fields.source, fields.dest)
                 }
                 catch (e) {
                     console.log(e);
                 }
             });
         }
-
+        else if (fields.text) {
+            vocabulary = await translate(fields.text, fields.source, fields.dest)
+        }
+        else {
+            const text = parseWebsite(fields.url)
+            vocabulary = await translate(text, fields.source, fields.dest)
+        }
+        return res.status(200)
+            .attachment(`vocabulary.csv`)
+            .send(vocabulary)
+            // if (!toTranslate || toTranslate.type != 'application/pdf') {
+        //     return res.status(400).render('error', {
+        //         message: 'Either no file was submitted or the format of the submitted file was not supported.',
+        //         error: {
+        //             status: 400,
+        //             stack: 'Supported file types right now are limited to pdf files.'
+        //         }
+        //     })
+        // }    
     })
 }
 
