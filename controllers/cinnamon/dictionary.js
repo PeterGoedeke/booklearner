e23d2941973c1e11c20d8ff1a8e557eb124b8237
Peter Goedeke <peterbgoedeke@gmail.com>
2021-01-16 18:59:59 +1300

    feat: added dictionary module

---
+++ controllers/cinnamon/dictionary.js
@@ -0,0 +1,7 @@
+const de = new Set(require('./de.json'))
+const es = new Set(require('an-array-of-spanish-words'))
+const en = new Set(require('an-array-of-english-words'))
+
+module.exports = {
+    de, es, en
+}
