865be1d0a39730a030a1a5f5b137c221fa192d26
PeterGoedeke <peterbgoedeke@gmail.com>
2021-01-20 15:41:58 +1300

    feat: added error handling to web scraping

--- controllers/cinnamon/parseWebsite.js
+++ controllers/cinnamon/parseWebsite.js
@@ -1,6 +1,7 @@
 // library dependencies
 const axios = require('axios')
 const { JSDOM } = require('jsdom')
+const validateUrl = require('url-validator')
 
 const getTextNodes = dom => {
     const body = dom.window.document.querySelector('body')
@@ -15,10 +16,21 @@ const getTextNodes = dom => {
 }
 
 const getWebsiteText = async url => {
-    const response = await axios.get(url)
-    const dom = new JSDOM(response.data)
-    const text = getTextNodes(dom).map(node => node.textContent).join('\n')
-    return text
+    const parsedUrl = validateUrl(url)
+    if (!parsedUrl) {
+        return null
+    }
+    try {
+        const response = await axios.get(parsedUrl)
+        const dom = new JSDOM(response.data)
+        const text = getTextNodes(dom).map(node => node.textContent).join('\n')
+        return text
+    }
+    catch (e) {
+        console.log(e)
+        throw e
+    }
 }
+
 
 module.exports = getWebsiteText
