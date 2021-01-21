6a02703ce344a97ce5ad8d8d7ee183d337695c1f
Peter Goedeke <peterbgoedeke@gmail.com>
2021-01-21 21:38:59 +1300

    feat: support for Russian language

--- controllers/cinnamon/dictionary.js
+++ controllers/cinnamon/dictionary.js
@@ -1,7 +1,8 @@
 const de = new Set(require('./de.json'))
 const es = new Set(require('an-array-of-spanish-words'))
 const en = new Set(require('an-array-of-english-words'))
+const ru = new Set(require('russian-words'))
 
 module.exports = {
-    de, es, en
+    de, es, en, ru
 }
