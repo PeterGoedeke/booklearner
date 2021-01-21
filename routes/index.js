61fc7fce9557f922703776d606e2348a1cef5c44
Peter Goedeke <peterbgoedeke@gmail.com>
2021-01-21 21:38:02 +1300

    feat: language display sourced from dictionary files

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,11 @@
 const router = require('express').Router()
 const controller = require('../controllers/index')
+const dictionary = require('../controllers/cinnamon/dictionary')
+
+const supportedLanguages = Object.getOwnPropertyNames(dictionary)
 
 router.get('/', (req, res) => {
-    res.render('index', {})
+    res.render('index', { supportedLanguages: supportedLanguages })
 })
 
 router.post('/submit', controller.translate)
