1c03bb59e0c49ce52c2b934088e2332f91f7630b
PeterGoedeke <peterbgoedeke@gmail.com>
2021-01-22 16:47:37 +1300

    refactor: textToWords refactor to separate unique filter

--- controllers/cinnamon/parseText.js
+++ controllers/cinnamon/parseText.js
@@ -6,20 +6,27 @@ const alphas = xregexp("[^\\s\\p{Latin}]+", "g");
 // dictionaries
 const dictionary = require('./dictionary')
 
+const countWords = ws => {
+    const counts = {}
+    ws.forEach(w => counts[w] ? counts[w] ++ : counts[w] = 1)
+    return counts
+}
+
 const uniq = arr => Array.from(new Set(arr))
 
-const textToWords = (source, s) =>
-    uniq(
-        s.replace(/\s\s+/g, ' ')
-         .trim()
-         .split(/\s/)
-         .map(w => xregexp.replace(w, alphas, ""))
-    )
+const parseText = (source, s) => s
+    .replace(/\s\s+/g, ' ')
+    .trim()
+    .split(/\s/)
+    .map(w => xregexp.replace(w, alphas, ""))
     .filter(w => dictionary[source].has(source == 'de' ? w : w.toLowerCase()))
+
+const textToWords = (source, s) => uniq(parseText(source, s))
 
 const wordsToQueries = ws => splitEvery(Number(process.env.WORDS_PER_QUERY), ws).map(te => te.join(','))
 
 module.exports = {
     textToWords,
-    wordsToQueries
+    wordsToQueries,
+    countWords
 }
